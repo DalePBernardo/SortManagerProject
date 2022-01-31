ee0787b078f7f9a7a848addaa57b330c49867a26
DalePBernardo <dalepb18@gmail.com>
2022-01-31 18:24:41 +0000

    Implemented the JUnit test cases for the Bubble Sort method

--- src/main/java/com/spartaglobal/sortmanager/BubbleSort.java
+++ src/main/java/com/spartaglobal/sortmanager/BubbleSort.java
@@ -1,13 +1,17 @@
 package com.spartaglobal.sortmanager;
 
 public class BubbleSort {
-    public int[] BubbleSort(int[] intArray){
-        for (int i = 0; i < intArray.length - 1; i++){
-            for (int j = 0; j < intArray.length - i - 1; j++){
-                if (intArray[j] > intArray[j+1]){
+
+    public static int[] bubbleSort(int[] intArray) {
+        if (intArray.length == 0)
+            return null;
+
+        for (int i = 0; i < intArray.length - 1; i++) {
+            for (int j = 0; j < intArray.length - i - 1; j++) {
+                if (intArray[j] > intArray[j + 1]) {
                     int temp = intArray[j];
-                    intArray[j] = intArray[j+1];
-                    intArray[j+1] = temp;
+                    intArray[j] = intArray[j + 1];
+                    intArray[j + 1] = temp;
                 }
             }
         }
--- src/main/java/com/spartaglobal/sortmanager/Main.java
+++ src/main/java/com/spartaglobal/sortmanager/Main.java
@@ -2,11 +2,11 @@ package com.spartaglobal.sortmanager;
 
 public class Main {
     public static void main(String[] args) {
-        // COPY ARRAY
         BubbleSort bs = new BubbleSort();
 
         int[] x = {9, 2, 4, 10, 42, 12, 0, 23};
-        x = bs.BubbleSort(x);
+        int[] y = x.clone();
+        x = bs.bubbleSort(x);
 
         for(int num : x)
             System.out.print(num + ", ");
